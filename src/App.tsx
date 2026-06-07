2cc0c95182774257e49a84970df474a362a4de84
Eldrich Salonga <eldrich.salonga@gmail.com>
2026-06-07 14:18:19 +0800

    add a debounce in products

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import { products } from "./data/products";
 import type { Product, Category, OrderItem } from "./types/types";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
@@ -12,23 +12,31 @@ const App = () => {
 
   const [search, setSearch] = useState<string>("");
   const [order, setOrder] = useState<OrderItem[]>([]);
+  const [product, setProduct] = useState<Product[]>(products);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setProduct(
+        products.filter((product: Product) => {
+          const matchesCategory =
+            selectedCategory === "All" || product.category === selectedCategory;
 
-  const filteredProducts: Product[] = products.filter((product: Product) => {
-    const matchesCategory =
-      selectedCategory === "All" || product.category === selectedCategory;
+          const matchesSearch = product.name
+            .toLowerCase()
+            .includes(search.toLowerCase());
 
-    const matchesSearch = product.name
-      .toLowerCase()
-      .includes(search.toLowerCase());
+          return matchesCategory && matchesSearch;
+        }),
+      );
+    }, 500);
 
-    return matchesCategory && matchesSearch;
-  });
+    return () => clearTimeout(timeout);
+  }, [search, selectedCategory]);
 
   return (
     <div className="space-y-3 select-none">
       <div className="border-b border-gray-300">
-        <Header search={search} setSearch={setSearch} order={order}/>
+        <Header search={search} setSearch={setSearch} order={order} />
       </div>
       <div className="grid grid-cols-[160px_1fr] gap-4">
         <Categories
@@ -40,9 +48,15 @@ const App = () => {
             <h3 className="font-bold text-lg">All Menu</h3>
             <p>Delicious food for you</p>
           </div>
-          {filteredProducts.length ? (
-            filteredProducts.map((product: Product) => {
-              return <ProductCard key={product.id} product={product} setOrder={setOrder}/>;
+          {product.length ? (
+            product.map((product: Product) => {
+              return (
+                <ProductCard
+                  key={product.id}
+                  product={product}
+                  setOrder={setOrder}
+                />
+              );
             })
           ) : (
             <div className="col-span-full flex items-center justify-center">
