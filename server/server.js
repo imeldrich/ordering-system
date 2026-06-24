008f3508234484f9131b48e812dc9e8220b1fd54
Eldrich Salonga <eldrich.salonga@gmail.com>
2026-06-24 18:56:25 +0800

    add a PORT in server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
-app.use(cors())
+app.use(cors());
 
 app.get("/api/products", (req, res) => {
   res.json([
@@ -64,6 +64,8 @@ app.get("/api/products", (req, res) => {
   ]);
 });
 
-app.listen(5000, () => {
-  console.log("Server listening on port: 5000...");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
 });
